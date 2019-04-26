004b754285a59f24fe2428ba0f8812c1b6645c72
mfreema1 <mfreema1@stevens.edu>
2019-04-26 14:57:14 -0400

    Pass test cases

--- RubyStarter/Tests/tc_US01.rb
+++ RubyStarter/Tests/tc_US01.rb
@@ -4,15 +4,15 @@ require "test/unit"
 class TestUS01 < Test::Unit::TestCase
 
     def test_simple_addition
-        assert_equal([], US01.new(2, 2, 4))
+        assert_equal([], US01.new().get_results(2, 2, 4))
     end
 
     def test_complex_addition
-        assert_equal([], US01.new(158, 413, 571))
+        assert_equal([], US01.new().get_results(158, 413, 571))
     end
 
     def test_wrong_addition
-        assert_equal([2, 2, 5], US01.new(2, 2, 5))
+        assert_equal([2, 2, 5], US01.new().get_results(2, 2, 5))
     end
 
 end
